e90772ebc4f9571e9df18f038946b8a8bd2f361d
Evgeni Ni <jounts@gmai.com>
2020-12-16 18:00:22 +0300

    db_prepare fixed user_exists function added account_exists function

--- auth/db.py
+++ auth/db.py
@@ -40,7 +40,7 @@ def db_prepare(conn: Connector) -> None:
         exec_msg = "CREATE TABLE auth_user(" \
                    f"{conf.AUTH_F_USERNAME} varchar(50) NOT NULL," \
                    f"{conf.AUTH_F_PASSWORD} varchar(250) NOT NULL," \
-                   f"{conf.AUTH_F_SID} varchar(10) NOT NULL," \
+                   f"{conf.AUTH_F_SID} varchar(20) NOT NULL," \
                    f"{conf.AUTH_F_SKILL} integer NOT NULL," \
                    f"PRIMARY KEY ({conf.AUTH_F_USERNAME}))"
         cursor.execute(exec_msg)
@@ -52,7 +52,7 @@ def insert_test_user(conn: Connector) -> None:
     :param conn: Connection object
     :return: None
     """
-    if user_exists(conf.NAME, conn):
+    if not user_exists(conf.NAME):
         with conn as cursor:
             exec_msg = f"INSERT INTO {conf.AUTH_TABLE_NAME}" \
                        f"({conf.AUTH_F_USERNAME}, {conf.AUTH_F_PASSWORD}, {conf.AUTH_F_SID}, {conf.AUTH_F_SKILL}) " \
@@ -60,21 +60,33 @@ def insert_test_user(conn: Connector) -> None:
             cursor.execute(exec_msg, (conf.NAME, conf.PASSWORD, '-1', conf.SKILL))
 
 
-def user_exists(username: str, conn: Connector) -> bool:
+def user_exists(username: str) -> bool:
     """
     Existing username in db function
     :param username: str
-    :param conn: Connection object
-    :return:
+    :return: bool
     """
+    conn = Connector()
     with conn as cursor:
-        exec_msg = f"SELECT username FROM {conf.AUTH_TABLE_NAME} where username = '{username}'"
-        cursor.execute(exec_msg)
-        exec_response = list(itertools.chain(*cursor.fetchall()))
-        if exec_response == username:
-            return True
+        exec_msg = f"SELECT {conf.AUTH_F_USERNAME} FROM {conf.AUTH_TABLE_NAME} where username = %s"
+        cursor.execute(exec_msg, (username,))
+        return cursor.fetchone() is not None
 
-        return False
+
+def account_exists(username: str, password: str) -> bool:
+    """
+    Existing account in db function
+    :param username: str
+    :param password: str
+    :return: bool
+    """
+    conn = Connector()
+    with conn as cursor:
+        exec_msg = f"select {conf.AUTH_F_USERNAME} " \
+                   f"from {conf.AUTH_TABLE_NAME} " \
+                   f"where {conf.AUTH_F_USERNAME} = %s and {conf.AUTH_F_PASSWORD} = %s"
+        cursor.execute(exec_msg, (username, password))
+        return cursor.fetchone() is not None
 
 
 if __name__ == '__main__':
