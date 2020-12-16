21faa18d7ddc510e58619a5fa27674405cdfbd20
Evgeni Ni <jounts@gmai.com>
2020-12-16 15:52:28 +0300

    db_prepare done

---
+++ auth/db_prepare.py
@@ -0,0 +1,59 @@
+"""
+db check and prepare
+"""
+
+import itertools
+
+from common.db import Connector
+from common import conf
+
+
+def db_check() -> None:
+    """
+    DB check and prepare for auth app function
+    :return: None
+    """
+    conn = Connector()
+
+    with conn as cursor:
+        cursor.execute(f"SELECT table_name "
+                       f"FROM information_schema.tables "
+                       f"WHERE table_schema = 'public' "
+                       f"ORDER BY table_name")
+
+        response = list(itertools.chain(*cursor.fetchall()))
+
+        if conf.AUTH_TABLE_NAME not in response:
+            db_prepare(conn)
+
+        if conf.TEST_KEY:
+            insert_test_user(conn)
+
+
+def db_prepare(conn: Connector) -> None:
+    """
+    Prepare DB for auth app function
+    :param conn: Connection object
+    :return: None
+    """
+    with conn as cursor:
+        exec_msg = "CREATE TABLE auth_user(" \
+                   f"{conf.AUTH_F_USERNAME} varchar(50) NOT NULL," \
+                   f"{conf.AUTH_F_PASSWORD} varchar(250) NOT NULL," \
+                   f"{conf.AUTH_F_SID} varchar(10) NOT NULL," \
+                   f"{conf.AUTH_F_SKILL} integer NOT NULL," \
+                   f"PRIMARY KEY ({conf.AUTH_F_USERNAME}))"
+        cursor.execute(exec_msg)
+
+
+def insert_test_user(conn: Connector) -> None:
+    """
+    Insert test user into db function
+    :param conn: Connection object
+    :return: None
+    """
+    with conn as cursor:
+        exec_msg = f"INSERT INTO {conf.AUTH_TABLE_NAME}" \
+                   f"({conf.AUTH_F_USERNAME}, {conf.AUTH_F_PASSWORD}, {conf.AUTH_F_SID}, {conf.AUTH_F_SKILL}) " \
+                   f"VALUES (%s, %s, %s, %s)"
+        cursor.execute(exec_msg, (conf.NAME, conf.PASSWORD, '-1', conf.SKILL))
---
+++ common/conf.py
@@ -0,0 +1,16 @@
+"""
+Configfile for auth application
+"""
+
+# table
+AUTH_TABLE_NAME: str = 'auth_user'
+# fields
+AUTH_F_USERNAME: str = 'username'
+AUTH_F_PASSWORD: str = 'password'
+AUTH_F_SID: str = 'session_id'
+AUTH_F_SKILL: str = 'skill_lvl'
+# test user
+TEST_KEY: bool = True
+NAME: str = 'test_user'
+PASSWORD: str = 'test_pass'
+SKILL: int = 4
